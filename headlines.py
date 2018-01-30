f51aee785a5535d3786648ee25b6761f5ccd501c
Seow Khee Wei <kwseow@gmail.com>
2018-01-30 21:08:04 +0800

    initial commit

---
+++ headlines.py
@@ -0,0 +1,12 @@
+locals()# coding:utf-8
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def get_news():
+  return "No news is good news"
+
+if __name__ == "__main__":
+  app.run(port=5000, debug=True)
